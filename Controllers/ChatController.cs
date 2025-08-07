6f205962454e9842ace0422a007e2bb1101e9185
123Framework <balatozar@gmail.com>
2025-08-07 11:53:10 +0300

    Add files via upload

--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -1,9 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TweeterApp.Data;
 
 namespace TweeterApp.Controllers
 {
+
     public class ChatController : Controller
     {
+        /*
+        private readonly ApplicationDbContext _context;
+        public ChatController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var messages = await _context.Messages
+                .OrderBy(x => x.Timestamp)
+                .ToListAsync();
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Send(string username, string text)
+        {
+            if (!string.IsNullOrWhiteSpace(username) && !string.IsNullOrWhiteSpace(text) {
+
+            }
+        }
+        */
         public IActionResult Index()
         {
             return View();
